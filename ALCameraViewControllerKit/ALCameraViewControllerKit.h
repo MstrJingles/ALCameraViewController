c494c7b0553ceb6015feab28b6db51a5a179de19
Zwatschek, Martin <martin.zwatschek@dvag.com>
2016-01-15 08:39:41 +0100

    Carthage support added

---
+++ ALCameraViewControllerKit/ALCameraViewControllerKit.h
@@ -0,0 +1,17 @@
+//
+//  ALCameraViewControllerKit.h
+//  ALCameraViewControllerKit
+//
+//  Created by Zwatschek, Martin on 15.01.16.
+//  Copyright © 2016 zero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ALCameraViewControllerKit.
+FOUNDATION_EXPORT double ALCameraViewControllerKitVersionNumber;
+
+//! Project version string for ALCameraViewControllerKit.
+FOUNDATION_EXPORT const unsigned char ALCameraViewControllerKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ALCameraViewControllerKit/PublicHeader.h>
